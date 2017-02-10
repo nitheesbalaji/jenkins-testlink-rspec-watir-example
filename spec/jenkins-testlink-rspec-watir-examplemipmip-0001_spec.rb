872f0beb061a7c9bb4cfece7cbd514064c39c984
nitheesbalaji <nithees.balaji@binfex.com>
2017-02-10 21:13:16 +0530

    Update jenkins-testlink-rspec-watir-examplemipmip-0001_spec.rb
    Removed OS'es other than linux

--- spec/jenkins-testlink-rspec-watir-examplemipmip-0001_spec.rb
+++ spec/jenkins-testlink-rspec-watir-examplemipmip-0001_spec.rb
@@ -6,16 +6,10 @@ require 'yaml'
 @os ||= (
 	host_os = RbConfig::CONFIG['host_os']
 	case host_os
-	when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-		:windows
-	when /darwin|mac os/
-		:macosx
 	when /linux/
 		require 'headless'
 		$headless = Headless.new
 		$headless.start
-	when /solaris|bsd/
-		:unix
 	else
 		raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
 	end
